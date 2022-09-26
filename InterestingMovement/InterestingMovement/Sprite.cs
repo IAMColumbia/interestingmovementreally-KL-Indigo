7e0b20040a8fd8495fd82644fe54e57852d45939
KL-Indigo <kliggins@colum.edu>
2022-09-25 19:29:38 -0500

    provides the Sprite class with a skeleton

---
+++ InterestingMovement/InterestingMovement/Sprite.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InterestingMovement
+{
+    public class Sprite
+    {
+        private string TextureName;
+
+        public string texturename
+        {
+            get
+            {
+                return TextureName;
+            }
+            set
+            {
+                TextureName = value;
+            }
+        }
+        protected Game game;
+        protected float speed;
+        protected float rotation;
+        protected Vector2 location;
+        protected Vector2 direction;
+        protected Vector2 origin;
+        protected Texture2D texture;
+
+        public Sprite(Game game)
+        {
+            this.game = game;
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public virtual void LoadContent()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
